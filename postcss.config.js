dc63eab5e321d843e2f2b0c4d8c2916035b0a957
SilentDepth <silentdepth@outlook.com>
2019-11-14 18:42:26 +0800

    add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,14 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    '@fullhuman/postcss-purgecss': {
+      content: [
+        './src/**/*.vue',
+      ],
+      whitelist: [
+        'html',
+        'body',
+      ],
+    },
   },
 }
